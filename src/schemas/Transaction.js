953fd08b93d212503240486ac3c6bcd655cde26b
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-07-20 10:35:52 -0500

    Hotfix date validation in Transaction schema

--- src/schemas/Transaction.js
+++ src/schemas/Transaction.js
@@ -1,12 +1,13 @@
 import * as yup from "yup";
 import { Categories } from "../utils/common";
+import { add } from "date-fns";
 
 const TransactionSchema = yup.object().shape({
   amount: yup.number().typeError("amount must be numeric").required(),
   date: yup
     .date()
     .required()
-    .max(new Date() + 1, "only past dates are permitted"),
+    .max(add(new Date(), { days: 1 })),
   payee: yup.string().required(),
   description: yup.string().required(),
   category: yup
